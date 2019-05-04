bb7f3033883a2856f38b71b6e2eae54624b3b7c5
Sergey Lomachev <anoscillator@gmail.com>
2019-05-05 01:51:19 +0300

    add upgrades database init code, not tested

--- app/src/main/java/com/example/mylittlestartup/ClickerApplication.java
+++ app/src/main/java/com/example/mylittlestartup/ClickerApplication.java
@@ -7,6 +7,11 @@ import android.content.Context;
 
 import com.example.mylittlestartup.data.api.ApiRepository;
 import com.example.mylittlestartup.data.sqlite.DBRepository;
+import com.example.mylittlestartup.data.sqlite.Upgrade;
+import com.example.mylittlestartup.data.sqlite.UpgradeDao;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClickerApplication extends Application {
     private ApiRepository mApiRepository;
@@ -20,6 +25,21 @@ public class ClickerApplication extends Application {
                 .allowMainThreadQueries() // TODO() don't forget to remove this line in release! Only for debug purpose!
                 .fallbackToDestructiveMigration()
                 .build();
+
+        addBasicUpgrades();
+    }
+
+    public void addBasicUpgrades() {
+        UpgradeDao dao = mDbRepository.getUpgradeDao();
+        List<Upgrade> upgrades = dao.all();
+        if (upgrades.isEmpty()) {
+            Upgrade[] basicUpgrades = {
+                    new Upgrade(1, 100, "hello", "world", 0, 1000, R.drawable.ic_launcher_background),
+                    new Upgrade(2, 1000, "manager", "", 0, 10000, R.drawable.ic_launcher_foreground)
+            };
+
+            dao.insert(basicUpgrades);
+        }
     }
 
     public ApiRepository getApis() {
