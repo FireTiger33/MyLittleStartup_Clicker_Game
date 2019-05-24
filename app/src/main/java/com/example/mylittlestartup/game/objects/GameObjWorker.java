5151c6be0859a964e5fadd9a163572d5b85e398b
FireTiger33 <32743837+FireTiger33@users.noreply.github.com>
2019-05-24 10:50:39 +0300

    - fix RunningGameObj pause/resume - small improvements upgrade dialog

--- app/src/main/java/com/example/mylittlestartup/game/objects/GameObjWorker.java
+++ app/src/main/java/com/example/mylittlestartup/game/objects/GameObjWorker.java
@@ -35,7 +35,7 @@ public class GameObjWorker extends Upgrade {
     private TextView nextValueTextView;
     private TextView nextIntervalTextView;
     private TextView priceView;
-    private boolean enoughMoneyForNextUpgrade;
+    private boolean needRecreateUpgradeDialog = true;
 
 
     public GameObjWorker(View itemView, View upgradeDialogView, int[] endCoordinateForPushAnimation, Upgrade upgrade, final GameContract.Presenter presenter) {
@@ -73,9 +73,6 @@ public class GameObjWorker extends Upgrade {
             nextIntervalTextView = mUpgradeDialogView.findViewById(R.id.next_interval);
             priceView = mUpgradeDialogView.findViewById(R.id.price_worker);
         }
-        /*if (mUpgradeDialogView.getParent() != null) {
-            ((ViewGroup)mUpgradeDialogView.getParent()).removeView(mUpgradeDialogView);
-        }*/
         nextLVLTextView.setText(new StringBuilder("lvl: " + mUpgrade.getCount() + " -> " + (mUpgrade.getCount() + 1)));
         nextValueTextView.setText(new StringBuilder("value: " + mUpgrade.getValue() + " -> " + (mUpgrade.getValue() * 2 + 100)));
         nextIntervalTextView.setText(new StringBuilder("pushInterval: " + mUpgrade.getInterval() + " -> " + (mUpgrade.getInterval() + 5000)));
@@ -83,24 +80,27 @@ public class GameObjWorker extends Upgrade {
         mPresenter.checkEnoughMoney(mUpgrade.getPrice(), new BaseCallback() {
             @Override
             public void onSuccess() {
-                enoughMoneyForNextUpgrade = true;
+                priceView.setTextColor(Color.GREEN);
             }
 
             @Override
             public void onError() {
-                enoughMoneyForNextUpgrade = false;
+                priceView.setTextColor(Color.RED);
             }
         });
-        if (enoughMoneyForNextUpgrade) {
-            priceView.setTextColor(Color.GREEN);
-        }
     }
 
-    private void showUpgradeDialog() {
-        Log.d(tag, "createUpgradeDialog");
-        if (upgradeDialog == null) {
-            Log.d(tag, "createDialog == null");
-            upgradeDialog = new AlertDialog.Builder(mItemView.getContext());
+    private void createUpgradeDialog() {
+        upgradeDialog = new AlertDialog.Builder(mItemView.getContext());
+        if (mUpgrade.getCount() == 0) {
+            upgradeDialog.setPositiveButton(R.string.hire, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.d(tag, "clickUpgradeWorker");
+                    mPresenter.onUpgradeWorker(mUpgrade);
+                }
+            });
+        } else {
             upgradeDialog.setPositiveButton(R.string.promote, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
@@ -115,20 +115,30 @@ public class GameObjWorker extends Upgrade {
                     mPresenter.onLayOffWorker(mUpgrade);
                 }
             });
-            upgradeDialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) { }
-            });
-            upgradeDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                @Override
-                public void onDismiss(DialogInterface dialog) {
-                    Log.d(tag, "upgradeDialogDismiss");
-                    if (mUpgradeDialogView.getParent() != null) {
-                        ((ViewGroup)mUpgradeDialogView.getParent()).removeView(mUpgradeDialogView);
-                    }
-                    mPresenter.onCloseWorkerUpgradeDialog();
+        }
+        upgradeDialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        upgradeDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                Log.d(tag, "upgradeDialogDismiss");
+                if (mUpgradeDialogView.getParent() != null) {
+                    ((ViewGroup) mUpgradeDialogView.getParent()).removeView(mUpgradeDialogView);
                 }
-            });
+                mPresenter.onCloseWorkerUpgradeDialog();
+            }
+        });
+        needRecreateUpgradeDialog = false;
+    }
+
+    private void showUpgradeDialog() {
+        Log.d(tag, "createUpgradeDialog");
+        if (needRecreateUpgradeDialog) {
+            Log.d(tag, "createDialog == null");
+            createUpgradeDialog();
         }
         recreateDialogViews();
         upgradeDialog.setView(mUpgradeDialogView);
@@ -140,6 +150,9 @@ public class GameObjWorker extends Upgrade {
     public void onUpgraded(Upgrade upgrade) {
         mUpgrade = upgrade;
         Log.d(tag, "onUpgraded");
+        if (mUpgrade.getCount() == 1) {
+            needRecreateUpgradeDialog = true;
+        }
         workTimer = new CountDownTimer(mUpgrade.getInterval(), mUpgrade.getInterval()) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -176,9 +189,13 @@ public class GameObjWorker extends Upgrade {
         pushAnimation.setInterpolator(new AccelerateInterpolator());
         pushAnimation.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void onAnimationStart(Animation animation) { }
+            public void onAnimationStart(Animation animation) {
+            }
+
             @Override
-            public void onAnimationRepeat(Animation animation) { }
+            public void onAnimationRepeat(Animation animation) {
+            }
+
             @Override
             public void onAnimationEnd(Animation animation) {
                 pushButton.setVisibility(View.INVISIBLE);
@@ -187,10 +204,6 @@ public class GameObjWorker extends Upgrade {
 
         });
         pushButton.startAnimation(pushAnimation);
-    }
-
-    private Upgrade getUpgrade() {
-        return mUpgrade;
     }
 
 }
